8602843557c61552e2733bf2575e6508d67840c7
Alvin <palifarec@gmail.com>
2021-02-02 14:36:54 +0900

    예매 js 2월 2일

---
+++ script/booking/booking.js
@@ -0,0 +1,13 @@
+// movie list toggle
+$(document).ready(function(){
+    $('#movie-list').children().click(function(){
+        if($(this).css("color") == "rgb(0, 0, 0)"){
+            $(this).css("background-color", "rgb(66, 66, 66)");
+            $(this).css("color", "rgb(255, 255, 255)");
+        }
+        else if($(this).css("color") == "rgb(255, 255, 255)"){
+            $(this).css("background-color", "rgb(217, 226, 225)");
+            $(this).css("color", "rgb(0, 0, 0)");
+        }
+    });
+});
