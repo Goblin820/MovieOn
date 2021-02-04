1184402f888eed7b3a4dca831f1ae31ef034db72
Goblin820 <ps911107@naver.com>
2021-02-04 16:27:34 +0900

    1. 로그인 스크립트 작성 2. 회원가입 레이아웃 & 디자인 작성중

--- script/login.js
+++ script/login.js
@@ -1,7 +1,21 @@
 const notScrollClassName = 'no-scroll';
 function OnLoginWindow() {
   document.body.classList.add(notScrollClassName);
+  LoginAppendFetch();
 }
 function OffLoginWindow() {
   document.body.classList.remove(notScrollClassName);
+  LoginRemove();
+}
+
+function LoginAppendFetch() {
+  fetch('../html/login.html').then(function (response) {
+    response.text().then(function (text) {
+      document.body.innerHTML += text;
+    });
+  });
+}
+function LoginRemove() {
+  const loginRoot = document.getElementById('login-root');
+  document.body.removeChild(loginRoot);
 }
