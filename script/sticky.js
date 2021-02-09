bdd669fea55849504c3b27729e8468ded885c39b
Goblin820 <ps911107@naver.com>
2021-02-10 08:36:46 +0900

    스티키 수정

--- script/sticky.js
+++ script/sticky.js
@@ -5,11 +5,11 @@ const block = document.getElementById('stickyBlock');
 window.addEventListener('scroll', () => {
   if (rectTop < window.pageYOffset) {
     nav.style.position = 'fixed';
-    nav.style.animation = 'down 0.5s ease';
+    // nav.style.animation = 'down 0.5s ease';
     block.style.display = 'block';
   } else {
     nav.style.position = 'sticky';
-    nav.style.animation = 'up 1s ease';
+    // nav.style.animation = 'up 1s ease';
     block.style.display = 'none';
   }
 });
