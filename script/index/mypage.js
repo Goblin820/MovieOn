5cf659929def5dc91433fd51f3e083a14bcecc2a
강재영 <procedure1010@gmail.com>
2021-02-10 02:32:55 +0900

    마이페이지 예매내역 commit

--- script/index/mypage.js
+++
@@ -1,5 +0,0 @@
-const movieName = localStorage.getItem('selectedName');
-const placeName = localStorage.getItem('selectedLocation');
-
-document.getElementById('MN').innerHTML = 'dddddddddd';
-document.getElementById('PN').innerHTML = placeName;
