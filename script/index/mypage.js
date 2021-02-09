6a3173871cca31616bdb3ed767d8d601cf182c7c
강재영 <procedure1010@gmail.com>
2021-02-10 02:06:36 +0900

    no message

---
+++ script/index/mypage.js
@@ -0,0 +1,5 @@
+const movieName = localStorage.getItem('selectedName');
+const placeName = localStorage.getItem('selectedLocation');
+
+document.getElementById('MN').innerHTML = 'dddddddddd';
+document.getElementById('PN').innerHTML = placeName;
